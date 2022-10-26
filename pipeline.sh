797a3e14fb9a5aba4ac0ac93d9d399b0130f35da
dyego <dyego@gmail.com>
2022-10-26 18:01:55 -0300

    testes gerais

--- pipeline.sh
+++ pipeline.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+JENKINS_USER=admin
+JENKINS_USER_API_TOKEN=118a20138166466ceba87231e31c533866
+JENKINS_ADDRESS="192.168.1.174:8080"
+
+
+BASIC_AUTH=${JENKINS_USER}:$JENKINS_USER_API_TOKEN
+
 git add .
 git commit -m "testes gerais"
 git push origin master
+
+curl http://${BASIC_AUTH}@${JENKINS_ADDRESS}/job/doc-jenkins/build?delay=0sec
